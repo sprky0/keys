4cbe09d2b161bd9ae9edf8bf7c17350e28cdde35
Avery Brooks <averyjamesbrooks@gmail.com>
2014-06-13 13:06:33 -0400

    get code, name

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+	grunt.file.defaultEncoding = 'utf8';
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Project configuration.
+	grunt.initConfig({
+		copy : {
+			main : {
+				src : "src/keys.js",
+				dest : "dist/keys.js"
+			}
+		},
+		uglify : {
+			main : {
+				files : {
+					'dist/index.min.js' : ['dist/index.js']
+				}
+			}
+		}
+	});
+
+	// dist build
+	grunt.registerTask('default', ['copy','uglify']);
+
+};
